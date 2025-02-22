461a372e24fc08aa9f1097eef03e6543988c77a8
trisston <ironcutterstudios@gmail.com>
2025-02-21 21:52:32 -0600

    Basic Start Menu Created

---
+++ jellydripcastle/scenes/Start Menu/start_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_button_button_down() -> void:
+	print("start")
+
+
+func _on_shop_button_button_down() -> void:
+	print("shop")
+
+
+func _on_exit_button_button_down() -> void:
+	print("exit")
