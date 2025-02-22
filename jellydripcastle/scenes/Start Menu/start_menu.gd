12f1f7a1d76905944bf87927ca13d2ed24a85651
trisston <ironcutterstudios@gmail.com>
2025-02-21 22:04:56 -0600

    Basic Menu Functionality

--- jellydripcastle/scenes/Start Menu/start_menu.gd
+++ jellydripcastle/scenes/Start Menu/start_menu.gd
@@ -1,13 +1,20 @@
 extends Control
 
+const MAIN = preload("res://scenes/Game/main_game.tscn")
+const SHOP = preload("res://scenes/Shop/shop.tscn")
 
 func _on_start_button_button_down() -> void:
-	print("start")
-
-
+	var main = MAIN.instantiate()
+	owner.add_child(main)
+	unload_menu()
+	
 func _on_shop_button_button_down() -> void:
-	print("shop")
-
+	var shop = SHOP.instantiate()
+	owner.add_child(shop)
+	unload_menu()
 
 func _on_exit_button_button_down() -> void:
-	print("exit")
+	get_tree().quit(0)
+
+func unload_menu():
+	get_node(".").queue_free()
